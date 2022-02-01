9b58cfc39ab6b32c120ba59f32807b79fd7b7342
uzytkownik <dima.mobil80@gmail.com>
2022-02-01 14:53:14 +0100

    Commit po stworzeniu glownych komponentow

---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Header = () => {
+    return <header className='header'>
+        <img src="https://media.istockphoto.com/photos/view-of-the-country-dirt-road-through-the-green-agricultural-fields-picture-id1225568390"></img>
+    </header>
+}
+
+
+export default Header
---
+++ src/components/Profile.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+
+const Profile = () => {
+    return <div className='content'>
+    <div>
+      <img src="https://cdn.pixabay.com/photo/2022/01/07/07/13/chicago-6921297__340.jpg"></img>
+    </div>
+    <div>
+      ava+discription
+    </div>
+      My posts
+      <div>
+        New Posts
+      </div>
+        <div>
+          <div>
+            post 1
+          </div>
+          <div>
+            post 2
+          </div>
+        </div>
+  </div>
+}
+
+export default Profile
